9844003676e39af681d76afcecf0808d21d2d95b
John Bretherton <270022735@bun2-204-16.acg.ac.nz>
2019-08-23 15:44:42 +1200

    Added contact input functionality

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         type: 'GET',
         dataType: 'json',
         success: function(data){
-            console.log(data);
+            // console.log(data);
             for (var i = 0; i < data.length; i++) {
                 $('#productBox').append(`<a class="list-group-item list-group-item-action">${data[i].name}<div class="buttonStyle"><button type="button" class="btn btn-info buttonSpace">Edit</button><button type="button" class="btn btn-danger">Remove</button></div></a>`);
             }
@@ -21,7 +21,7 @@ $(document).ready(function(){
 });
 
 $('#addProductButton').click(function(){
-    // check if user has input anything into the two input fields
+    // check if user has typed anything in the two input fields
     // console.log('button has been clicked');
     // prevent the default of submitting data and refreshing the page
     event.preventDefault();
@@ -54,5 +54,33 @@ $('#addProductButton').click(function(){
 });
 
 $('#contactButton').click(function(){
-    
+    event.preventDefault();
+    let userName = $('#userName').val();
+    let userEmail = $('#userEmail').val();
+    let userMessage = $('#userMessage').val();
+    // console.log(userName, userEmail, userMessage);
+    if(userName.length === 0){
+      console.log('please enter your name');
+    } else if(userEmail.length === 0){
+      console.log('please enter your email address');
+    } else if(userMessage.length === 0){
+      console.log('please write a message to us, kind sir or madam');
+    } else{
+      $.ajax({
+        url: 'http://192.168.33.10:3000/contact',
+        type: 'POST',
+        data: {
+          name: userName,
+          email: userEmail,
+          message: userMessage
+        },
+        success: function(result){
+          console.log(result);
+        },
+        error: function(error){
+          console.log(error);
+          console.log('something screwed up with sending user data');
+        }
+      })
+    }
 });

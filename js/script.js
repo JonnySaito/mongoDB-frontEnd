287e46b3f01c1af51b3e7ae186b9068382970fa4
John Bretherton <270022735@bun2-206-24.acg.ac.nz>
2019-09-03 11:35:22 +1200

    Register, login, add/edit/remove product, logout

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-// Make an Ajax request for all data (we have json) and give it as a list
 let serverURL;
 let serverPort;
 let url;
 let editing = false;
-// Get the JSON file
+
+// Get the JSON File
 $.ajax({
   url: 'config.json',
   type: 'GET',
@@ -12,31 +12,38 @@ $.ajax({
     serverURL = keys['SERVER_URL'];
     serverPort = keys['SERVER_PORT'];
     url = `${keys['SERVER_URL']}:${keys['SERVER_PORT']}`;
-    getProductData();
+    getProductsData();
   },
   error: function(){
     console.log('cannot find config.json file, cannot run application');
   }
 });
 
-getProductData = () => {
-    // console.log(`${serverURL}:${serverPort}/allProducts`);
+// Get all the products
+getProductsData = () => {
     $.ajax({
         // url: `${serverURL}:${serverPort}/allProducts`,
         url: `${url}/allProducts`,
         type: 'GET',
         dataType: 'json',
         success:function(data){
+            $('#productList').empty();
             for (var i = 0; i < data.length; i++) {
-                $('#productList').append(`
-                    <li class="list-group-item d-flex justify-content-between align-items-center" data-id="${data[i]._id}">
-                        <span class="productName">${data[i].name}</span>
-                        <div>
-                            <button class="btn btn-info editBtn">Edit</button>
-                            <button class="btn btn-danger removeBtn">Remove</button>
-                        </div>
-                    </li>
-                `);
+                let product = `
+                    <li
+                        class="list-group-item d-flex justify-content-between align-items-center productItem"
+                        data-id="${data[i]._id}"
+                    >
+                        <span class="productName">${data[i].name}</span>`;
+                        if(sessionStorage['userName']){
+                            product += `<div>
+                                            <button class="btn btn-info editBtn">Edit</button>
+                                            <button class="btn btn-danger removeBtn">Remove</button>
+                                        </div>`;
+                        }
+                    product += `</li>
+                `;
+                $('#productList').append(product);
             }
         },
         error: function(err){
@@ -45,26 +52,27 @@ getProductData = () => {
         }
     })
 }
-// ADD A PRODUCT
+
+//Add or Edit a product
 $('#addProductButton').click(function(){
     event.preventDefault();
     let productName = $('#productName').val();
     let productPrice = $('#productPrice').val();
     if(productName.length === 0){
-        console.log('Please enter a product name');
+        console.log('please enter a product name');
     } else if(productPrice.length === 0){
-        console.log('Please enter a product price');
+        console.log('please enter a product price');
     } else {
         if(editing === true){
             const id = $('#productID').val();
             $.ajax({
-                url: `${url}/product/${id}`,
+                url: `${url}/editProduct/${id}`,
                 type: 'PATCH',
                 data: {
                     name: productName,
                     price: productPrice
                 },
-                success: function(result){
+                success:function(result){
                     $('#productName').val(null);
                     $('#productPrice').val(null);
                     $('#productID').val(null);
@@ -75,17 +83,16 @@ $('#addProductButton').click(function(){
                     allProducts.each(function(){
                         if($(this).data('id') === id){
                             $(this).find('.productName').text(productName);
+                            return false;
                         }
                     });
                 },
-                error: function(error){
-                    console.log(error);
-                    console.log('Something went wrong with editing the product');
+                error: function(err){
+                    console.log(err);
+                    console.log('something went wrong with editing the product');
                 }
-            });
-                // console.log(`Edited ${productName} to be ${productPrice}`);
+            })
         } else {
-            console.log(`${productName} costs $${productPrice}`);
             $.ajax({
                 url: `${url}/product`,
                 type: 'POST',
@@ -97,7 +104,8 @@ $('#addProductButton').click(function(){
                     $('#productName').val(null);
                     $('#productPrice').val(null);
                     $('#productList').append(`
-                        <li class="list-group-item d-flex justify-content-between align-items-center productItem">
+                        <li class="list-group-item d-flex justify-content-between align-items-center productItem" data-id="${result._id}">
+
                             <span class="productName">${result.name}</span>
                             <div>
                                 <button class="btn btn-info editBtn">Edit</button>
@@ -112,19 +120,23 @@ $('#addProductButton').click(function(){
                 }
             })
         }
+
     }
-});
-// EDIT button to fill the form with existing product
+})
+
+// Edit button to fill the form with exisiting product
 $('#productList').on('click', '.editBtn', function() {
-    // console.log(`${serverURL}:${serverPort}/product/${id}`);
     event.preventDefault();
+    // Get the ID of the product we want to edit.
+    // We have saved the ID into data-id of the parent li of the button
     const id = $(this).parent().parent().data('id');
+    console.log(id);
+    // Send an Ajax request to our route for getting a single product
     $.ajax({
         url: `${url}/product/${id}`,
         type: 'get',
         dataType: 'json',
         success:function(product){
-            console.log(product);
             $('#productName').val(product['name']);
             $('#productPrice').val(product['price']);
             $('#productID').val(product['_id']);
@@ -134,12 +146,12 @@ $('#productList').on('click', '.editBtn', function() {
         },
         error:function(err){
             console.log(err);
-            console.log('Something went wrong with getting the single product');
+            console.log('something went wrong with getting the single product');
         }
     })
-
 });
-// REMOVE a product
+
+// Remove a product
 $('#productList').on('click', '.removeBtn', function(){
     event.preventDefault();
     const id = $(this).parent().parent().data('id');
@@ -147,7 +159,7 @@ $('#productList').on('click', '.removeBtn', function(){
     $.ajax({
       url: `${url}/product/${id}`,
       type: 'DELETE',
-      success: function(result){
+      success:function(result){
         li.remove();
       },
       error:function(err) {
@@ -156,38 +168,6 @@ $('#productList').on('click', '.removeBtn', function(){
       }
     })
 });
-
-// $('#contactButton').click(function(){
-//     event.preventDefault();
-//     let userName = $('#userName').val();
-//     let userEmail = $('#userEmail').val();
-//     let userMessage = $('#userMessage').val();
-//     // console.log(userName, userEmail, userMessage);
-//     if(userName.length === 0){
-//       console.log('please enter your name');
-//     } else if(userEmail.length === 0){
-//       console.log('please enter your email address');
-//     } else if(userMessage.length === 0){
-//       console.log('please write a message to us, kind sir or madam');
-//     } else{
-//       $.ajax({
-//         url: `${SERVER_URL}:${SERVER_PORT}/contact`,
-//         type: 'POST',
-//         data: {
-//           name: userName,
-//           email: userEmail,
-//           message: userMessage
-//         },
-//         success: function(result){
-//           console.log(result);
-//         },
-//         error: function(error){
-//           console.log(error);
-//           console.log('something screwed up with sending user data');
-//         }
-//       })
-//     }
-// });
 
 $('#loginTabBtn').click(function(){
     event.preventDefault();
@@ -206,109 +186,145 @@ $('#registerTabBtn').click(function(){
 
 });
 
+// Register Form
 $('#registerForm').submit(function(){
     event.preventDefault();
-    console.log('register button has been clicked');
+    // Get all the values from the input fields
     const username = $('#rUsername').val();
     const email = $('#rEmail').val();
     const password = $('#rPassword').val();
     const confirmPassword = $('#rConfirmPassword').val();
-    console.log(username);
-    console.log(email);
-    console.log(password);
-    console.log(confirmPassword);
+
+    // We are including basic validation
+    // Eventually we would need to include a more thorough validation (required, min, max values, emails, uniques, etc)
+    // For time's sake, we are just checking to see if there is a value in each input field
     if(username.length === 0){
         console.log('please enter a username');
-    } else if (email.length === 0) {
+    } else if(email.length === 0){
         console.log('please enter an email');
-    } else if (password.length === 0) {
+    } else if(password.length === 0){
         console.log('please enter a password');
-    } else if (confirmPassword.length === 0) {
+    } else if(confirmPassword.length === 0){
         console.log('please confirm your password');
-    } else if (password !== confirmPassword) {
+    } else if(password !== confirmPassword){
+        // We also need to check if the two passwords match
         console.log('your passwords do not match');
     } else {
+        // Once all the validation has passed we run our ajax request to our register route
         $.ajax({
-            url:`${url}/users`,
+            url: `${url}/users`,
             type: 'POST',
             data: {
                 username: username,
                 email: email,
                 password: password
             },
-            success: function(result){
+            success:function(result){
                 console.log(result);
             },
-            error: function(err){
+            error:function(err){
                 console.log(err);
-                console.log('something went wrong with registering a new user');
+                console.log('Something went wrong with registering a new user');
             }
         })
     }
 });
 
-// Click login button --> check if username is valid (Front-end validation)
+// Login Form
 $('#loginForm').submit(function(){
     event.preventDefault();
-    console.log('Login button has been clicked');
+    // Get the two input fields
     const username = $('#lUsername').val();
     const password = $('#lPassword').val();
-    console.log(username);
-    console.log(password);
+
+    // Add in the simple validation to make sure people input a value
     if(username.length === 0){
-        console.log('Please enter a username');
-    } else if (password.length === 0) {
-        console.log('Please enter a password');
+        console.log('please enter a username');
+    } else if(password.length === 0){
+        console.log('please enter a password');
     } else {
-        console.log('Username is OK!');
+        // Send an ajax request to our login route.
+        // Even though we are getting back a user, beacuse we are dealing with secure data (password), we want to use a POST request
         $.ajax({
-            url:`${url}/getUser`,
+            url: `${url}/getUser`,
             type: 'POST',
             data: {
                 username: username,
                 password: password
             },
-            success: function(result){
+            success:function(result){
+                // the result value is whatever gets sent back from the server.
                 if(result === 'invalid user'){
+                    // If someone tries to login with a username that doesnt exist
                     console.log('cannot find user with that username');
                 } else if(result === 'invalid password'){
-                    console.log('your password is wrong');
+                    // If someone logs in with a valid username but the password is wrong
+                    console.log('Your password is wrong');
                 } else {
-                    console.log('let us log you in');
+                    // If someone logs in with a valid username and a valid password
+                    console.log('lets log you in');
                     console.log(result);
 
-                    sessionStorage.setItem('userId', result['_id']);
+                    // sessionStorage (and LocalStorage) allows you to save data into your web browser and will stay there until they get removed
+                    // sessionStorage will keep data until the session is finsihed (closing the tab or browser)
+                    // localStorage will keep the data forever until someone manually clears the localStorage cache.
+                    // This is how we will be creating our login system
+                    // If we save a value into sessionStorage or localStorage, if we keep refreshing our page, the value we saved will still be there.
+                    // In our document.ready() function bellow we are checking to see if there is a value in our sessionStorage called user_Name
+                    sessionStorage.setItem('userID', result['_id']);
                     sessionStorage.setItem('userName', result['username']);
                     sessionStorage.setItem('userEmail', result['email']);
+                    getProductsData();
+                    $('#authForm').modal('hide');
+                    $('#loginBtn').hide();
+                    $('#logoutBtn').removeClass('d-none');
+                    $('#addProductSection').removeClass('d-none');
                 }
             },
-            error: function(err){
+            error:function(err){
                 console.log(err);
-                console.log('something went wrong with logging in');
+                console.log('Something went wrong with logging in.');
             }
         })
     }
 });
 
-// This below makes the modal appear on pageload; just while we're working on it; we'll turn it off later
-// $(document).ready(function(){
-//     $('#authForm').modal('show');
-//     console.log(sessionStorage);
-// });
 
-$('#loginButton').on('click', function(){
-    // showModal();
-    $('#authForm').modal('show');
-    // $('#loginButton').addClass('d-none');
-    $('#loginButton').hide();
-    $('#logoutButton').addClass('d-inline');
-    console.log(sessionStorage);
-});
-
-$('#logoutButton').on('click', function(){
-    // sessionStorage.clear('userId');
-    // sessionStorage.clear('userName');
-    // sessionStorage.clear('userEmail');
-    $('#authForm').modal('hide');
+$('#logoutBtn').click(function(){
     sessionStorage.clear();
+    getProductsData();
+    $('#loginBtn').show();
+    $('#logoutBtn').addClass('d-none');
+    $('#addProductSection').addClass('d-none');
+    $('#lUsername').val(null);
+    $('#lPassword').val(null);
 });
+
+//We are using this so that our modal appears on load
+//We will turn this off when we are ready
+$(document).ready(function(){
+    // This allows the modal to pop up on load (we will remove this line when we are done with the login / register functionality)
+    // $('#authForm').modal('show');
+
+    // Check to see if there is a value called user_Name in the sessionStorage, this will only be there when we login in successfully
+    if(sessionStorage['userName']){
+        // you have logged in
+        console.log('you are logged in ');
+        $('#loginBtn').hide();
+        $('#logoutBtn').removeClass('d-none');
+        $('#addProductSection').removeClass('d-none');
+
+    } else {
+        // you aren't logged in
+        console.log('please sign in');
+    }
+
+    // If you check sessionStorage when there isnt anything in there it should be an empty array
+    // If you check it when there is some values, it will be an object
+    console.log(sessionStorage);
+
+    // From here we are going to be using a lot of if statements to hide and show specifc elements.
+    // If there is a value for user_Name, then we will see the Logout button, but if there isn't then we will see the Login/Register button.
+    // to clear out sessionStorage we need to call. sessionStorage.clear() which will clear all the items in our session storage.
+    // This will happen on a click function for our logout button
+})
